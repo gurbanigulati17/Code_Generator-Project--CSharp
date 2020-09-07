3e8ea9931739778a9c69b794743ab00a14f02ba5
Gurubani Gulati <gurbanigulati17@gmail.com>
2020-09-07 21:40:18 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace BarCodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.txtEncode = new System.Windows.Forms.TextBox();
+            this.txtDecode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.pict = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.BackColor = System.Drawing.Color.Green;
+            this.btnEncode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncode.Location = new System.Drawing.Point(216, 381);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(112, 52);
+            this.btnEncode.TabIndex = 0;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = false;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Encode:";
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.Color.White;
+            this.pic.Location = new System.Drawing.Point(41, 32);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(178, 148);
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // txtEncode
+            // 
+            this.txtEncode.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEncode.Location = new System.Drawing.Point(157, 267);
+            this.txtEncode.Multiline = true;
+            this.txtEncode.Name = "txtEncode";
+            this.txtEncode.Size = new System.Drawing.Size(289, 32);
+            this.txtEncode.TabIndex = 3;
+            // 
+            // txtDecode
+            // 
+            this.txtDecode.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecode.Location = new System.Drawing.Point(157, 319);
+            this.txtDecode.Multiline = true;
+            this.txtDecode.Name = "txtDecode";
+            this.txtDecode.Size = new System.Drawing.Size(289, 30);
+            this.txtDecode.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Decode:";
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.BackColor = System.Drawing.Color.Green;
+            this.btnDecode.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecode.Location = new System.Drawing.Point(334, 381);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(112, 52);
+            this.btnDecode.TabIndex = 6;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = false;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // pict
+            // 
+            this.pict.BackColor = System.Drawing.Color.White;
+            this.pict.Location = new System.Drawing.Point(268, 32);
+            this.pict.Name = "pict";
+            this.pict.Size = new System.Drawing.Size(178, 148);
+            this.pict.TabIndex = 7;
+            this.pict.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(41, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 49);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "QR Code";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Goldenrod;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(268, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 49);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Bar Code";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
+            this.ClientSize = new System.Drawing.Size(498, 445);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pict);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.txtDecode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEncode);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEncode);
+            this.Name = "Form1";
+            this.Text = "CODE GENERATOR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.TextBox txtEncode;
+        private System.Windows.Forms.TextBox txtDecode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.PictureBox pict;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿#region Assembly zxing, Version=0.16.5.0, Culture=neutral, PublicKeyToken=4e88037ac681fe60
+// C:\Users\geetg\source\repos\BarCodeGenerator\packages\ZXing.Net.0.16.5\lib\net47\zxing.dll
+#endregion
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+
+
+namespace BarCodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            var reader = new ZXing.BarcodeReader();
+            var result = reader.Decode((Bitmap)pic.Image);
+            if (result != null)
+            {
+                txtDecode.Text = result.ToString();
+            }
+
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            BarcodeWriter writer = new BarcodeWriter() { Format = BarcodeFormat.QR_CODE };
+            pic.Image = writer.Write(txtEncode.Text);
+            BarcodeWriter writer1 = new BarcodeWriter() { Format = BarcodeFormat.CODE_128 };
+            pict.Image = writer1.Write(txtEncode.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
